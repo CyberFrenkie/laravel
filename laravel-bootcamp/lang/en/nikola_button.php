c6d1107c02487d37319422fdf167b430304d93c3
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-07-15 21:59:40 +0200

    ovako mnogo lakse

---
+++ laravel-bootcamp/lang/en/nikola_button.php
@@ -0,0 +1,15 @@
+<?php
+return[
+    'button1' =>[
+        'url' =>'https://www.youtube.com/watch?v=qbaijAjSDGE',
+        'text'  => 'a bop',
+        'class' => 'btn btn-dark',
+        'target' => '_blank',
+    ],
+    'button2' =>[
+        'url' =>'https://www.youtube.com/watch?v=JPSnFknRXX4',
+        'text'  => 'another bop',
+        'class' => 'btn btn-dark',
+        'target' => '_blank',
+    ],     
+];
--- laravel-bootcamp/resources/views/nikola.blade.php
+++ laravel-bootcamp/resources/views/nikola.blade.php
@@ -3,11 +3,16 @@
 @section('content')
     
     <div class="wrapper">
-
-        <h1>{{ trans('home.page.persons.1.name') }}</h1>
-        
-
-
+        <h1>{{ trans('nikola.content1.title') }}</h1>
+        @foreach (trans('nikola.content1.paragraphs') as $subtitles)
+            <p>{{ $subtitles['subtitle']}}</p>
+        @endforeach
+        <h1>{{ trans('nikola.content2.title') }}</h1>
+        @foreach (trans('nikola.content2.paragraphs') as $subtitles)
+            <p>{{ $subtitles['subtitle']}}</p>
+        @endforeach
+        <a href="{{ trans('nikola_button.button1.url') }}" target="{{trans('nikola_button.button1.target')}}" class="{{trans('nikola_button.button1.class')}}" >{{trans('nikola_button.button1.text')}}</a>
+        <a href="{{ trans('nikola_button.button2.url') }}" target="{{trans('nikola_button.button2.target')}}" class="{{trans('nikola_button.button2.class')}}" >{{trans('nikola_button.button2.text')}}</a>
     </div>
 
 @endsection
