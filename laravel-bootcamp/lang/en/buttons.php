a7b4acd1875627d97d5222ab6fc32cfd716c85fb
Peconji <pecapeconji@gmail.com>
2023-07-18 02:05:59 +0200

    added required lang files

--- laravel-bootcamp/lang/en/buttons.php
+++ laravel-bootcamp/lang/en/buttons.php
@@ -11,6 +11,17 @@ return [
         'url' => 'https://www.example.com',
         'target' => '_blank',
     ],
+    // ne znam da li sam failao ovo sad ali dodavacu komentare da bi znali gde je greska ako je bude bilo
+    
+    // for second HW
+    'gif'=> [
+        'text' => 'Kishni Gif',
+        'url' => 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2dtOHFzaWY1Y3psamFoMnFrMTdid3ZvazFubGF4OGZiNWF0OWw1byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sirUEemIYs5jBujwia/giphy-downsized-large.gif',
+        'target' => '_blank'
+    ],
+    'drvo' => [
+        'text' => 'Drvo',
+        'url' => 'https://i.imgur.com/uSkogBH.jpg',
+        'target' => '_blank',
+    ],
 ];
-
-// ne znam da li sam failao ovo sad ali dodavacu komentare da bi znali gde je greska ako je bude bilo
---
+++ laravel-bootcamp/lang/en/story.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'content' => [
+        'story1' => 'A long time ago in a little pretty town called Majdanhole, lived a person named Shaco. He was known throughout the town as a mysterious and mischievous individual. Shaco possessed extraordinary skills in stealth and cunning, making him a formidable force to reckon with. However, his heart was filled with darkness, and he used his talents for malevolent purposes.
+
+        Majdanhole was a peaceful and harmonious place, where the townspeople lived happily and harmoniously. But when Shaco arrived, the tranquility of the town began to crumble. He offered his services as an assassin, taking payments to eliminate those who crossed his clients paths.
+        
+        News of Shaco\'s presence spread like wildfire through Majdanhole, instilling fear and despair in the hearts of its inhabitants. People grew suspicious of one another, never knowing who might have hired the infamous assassin. The once cheerful atmosphere transformed into a somber and distrustful ambiance.
+        
+        However, not everyone in Majdanhole was content to live in fear. Among the townsfolk, there was a young girl named Lily, known for her bravery and compassion. She refused to accept the darkness that had befallen her home.
+        
+        Determined to restore peace to Majdanhole, Lily embarked on a quest to find a legendary artifact said to have the power to vanquish evil. Through ancient tales passed down by the village elders, she learned of the mystical Amulet of Harmony. Legend had it that this amulet could neutralize even the darkest of forces and restore balance to the land.
+        
+        With hope in her heart, Lily ventured into the depths of the enchanted forest, where the amulet was believed to be hidden. Along her journey, she encountered various challenges and obstacles, but her determination never wavered.
+        
+        Finally, after days of tireless searching, Lily discovered a hidden cave bathed in shimmering light. Inside, she found the Amulet of Harmony resting upon a pedestal. As she grasped the amulet, a surge of power coursed through her veins, filling her with confidence and courage.
+        
+        Returning to Majdanhole, Lily confronted Shaco, who had continued his ruthless activities unabated. Their encounter was a clash between darkness and light, as Shaco relied on his stealth and trickery, while Lily channeled the strength of the amulet and her unwavering resolve.
+        
+        In the heat of battle, Lily\'s compassion and bravery proved to be more potent than Shaco\'s malevolence. With each strike, the amulet unleashed a burst of harmony, weakening Shaco\'s dark powers. Lily\'s unwavering belief in the goodness of others also began to penetrate Shaco\'s heart, causing doubt to creep into his once confident demeanor.
+        
+        As the battle reached its climax, Shaco found himself at a crossroads.',
+        
+
+        'story2' => 'The darkness within him clashed with the glimmers of light emanating from Lily\'s presence. Overwhelmed by the realization of the pain he had caused, Shaco made a choice that would forever change his destiny.
+        
+        With a repentant heart, Shaco surrendered to Lily, pledging to use his skills for good instead of evil. Majdanhole rejoiced as the news of Shaco\'s change of heart spread through the town. The once-distrustful townsfolk began to rebuild their relationships, and a newfound harmony filled the air.
+        
+        From that day forward, Shaco dedicated his life to protecting the people of Majdanhole. He used his unique skills to guard the town against external threats and to ensure the safety and happiness of its inhabitants. The memory of his past misdeeds served as a constant reminder of the transformative power of compassion and redemption.
+        
+        And so, Majdanhole thrived, basking in the warmth of its restored harmony. The tale of Shaco and Lily became a legend, reminding future generations of the triumph of light over darkness and the capacity for change within even the most troubled souls.'
+    ],
+];
