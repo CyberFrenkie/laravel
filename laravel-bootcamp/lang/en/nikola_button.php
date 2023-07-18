eda4c22fd07109acf15a623878a029db5d83dbdc
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-07-18 19:35:44 +0200

    Revert "hw za sutra"
    This reverts commit d0a2546a1a48ef4c3dd7b6b19f24509543d296db.

--- laravel-bootcamp/lang/en/nikola_button.php
+++ laravel-bootcamp/lang/en/nikola_button.php
@@ -3,13 +3,13 @@ return[
     'button1' =>[
         'url' =>'https://www.youtube.com/watch?v=qbaijAjSDGE',
         'text'  => 'a bop',
-        'class' => 'btn btn-dark mt-2',
+        'class' => 'btn btn-dark',
         'target' => '_blank',
     ],
     'button2' =>[
         'url' =>'https://www.youtube.com/watch?v=JPSnFknRXX4',
         'text'  => 'another bop',
-        'class' => 'btn btn-light mt-2',
+        'class' => 'btn btn-dark',
         'target' => '_blank',
     ],     
 ];
