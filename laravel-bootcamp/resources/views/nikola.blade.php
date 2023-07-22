ee8f3aaaf600f8f250adec15435efbe2beb8b18b
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-07-22 23:32:14 +0200

    added the navbar as requested

--- laravel-bootcamp/resources/views/nikola.blade.php
+++ laravel-bootcamp/resources/views/nikola.blade.php
@@ -1,7 +1,24 @@
 @extends('layout')
 
 @section('content')
-    
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid test">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        @foreach( trans('nikola_nav.value.navBtn.btn') as $btn)
+        <li class="{{ __('nikola_nav.value.navBtn.textValue.classLi') }}">
+            <a href="{{ __('nikola_nav.value.navBtn.textValue.url') }}" class="{{ __('nikola_nav.value.navBtn.textValue.classA') }}">
+                {{ $btn }}
+            </a> 
+        @endforeach  
+      </ul>
+    </div>
+  </div>
+</nav>      
     <div class="wrapper">
         <h1>{!! trans('nikola.content1.title') !!}</h1>
         @foreach (trans('nikola.content1.paragraphs') as $subtitles)
@@ -20,5 +37,7 @@
             @endforeach 
         {{-- actually foreach the buttons --}} 
     </div>
+
+
 
 @endsection
