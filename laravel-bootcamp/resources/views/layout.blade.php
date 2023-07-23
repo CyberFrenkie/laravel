a3c6a294a9b5f33ea9c147efaaab52eafdaabb88
Sofija <sofijatomasevic05@gmail.com>
2023-07-23 20:11:48 +0200

    Gradient

--- laravel-bootcamp/resources/views/layout.blade.php
+++ laravel-bootcamp/resources/views/layout.blade.php
@@ -17,6 +17,7 @@
 
 
     <a class="" href="{{ __('home.page.return.url') }}">{{ __('home.page.return.name') }}</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
