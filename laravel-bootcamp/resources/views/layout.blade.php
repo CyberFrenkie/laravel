a9ae8daa3015474ae2c3a7e2b5123ceaf74960de
Sofija <sofijatomasevic05@gmail.com>
2023-07-23 19:32:58 +0200

    Linking Bootstrap

--- laravel-bootcamp/resources/views/layout.blade.php
+++ laravel-bootcamp/resources/views/layout.blade.php
@@ -17,5 +17,7 @@
 
 
     <a class="" href="{{ __('home.page.return.url') }}">{{ __('home.page.return.name') }}</a>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
