fa08ab76cd417f69f1bd517f51f618e800413b88
Peconji <pecapeconji@gmail.com>
2023-07-23 23:13:34 +0200

    navbar_created

--- laravel-bootcamp/lang/en/buttons.php
+++ laravel-bootcamp/lang/en/buttons.php
@@ -1,53 +1,41 @@
 <?php
 
 return [
-    'content' => [
-    'title' =>'<h1 class="tittle">Cemetery Gates</h1>',
-    'text' => [
-            ['lyrics' => 'Reverend, reverend
-            Is this some conspiracy?
-            Crucified for no sins
-            An image beneath me
-            Lost within our plans for life
-            It all seems so unreal
-            I\'m a man cut in half in this world
-            Left in my misery', ],
-            
-            ['lyrics' => 'Reverend, he turned to me
-            Without a tear in his eyes
-            Nothing new for him to see
-            I didn\'t ask him why', ],
-            
-            ['lyrics' => 'I will remember
-            The love our souls had sworn to make
-            Now I watch the falling rain
-            All my mind can see now is your',   ],
-            
-            ['lyrics' => 'Well, I guess you took my youth
-            And gave it all away
-            Like the birth of a new-found joy
-            This love would end in rage',   ],
-            
-            ['lyrics' => 'And when she died I couldn\'t cry
-            The pride was in my soul
-            You left me incomplete
-            All alone as the memories now unfold',],
+    'pre-headerContact' => [
+        //kreiram prvo dugme tel sa elementima koje cu da povucem u .blade.php file
+        'contact_tel' => [
+            'text' => '555-333',
+            'type' => 'tel',
+            'url' => 'tel:555-333',
         ],
-        
-        'buttons' => [
-            [
-                'text' => 'Cobalt',
-                'url' => 'https://www.youtube.com/watch?v=hUumqcL7f2o&t=8816s&ab_channel=EnterDystopiaRecords',
-                'target' => '_blank',
-                'class' => 'button primary-button',
-            ],
-            [
-                'text' => 'Emo Robot',
-                'url' => 'https://www.youtube.com/watch?v=rKhDxHxLALg&ab_channel=TesseracTstudio',
-                'target' => '_blank',
-                'class' => 'button secondary-button',
-            ],
+        //kreiram drugo dugme mail sa elementima koje cu da povucem u .blade.php file
+        'contact_mail' => [
+            'text' => 'nekimail@gmai.com',
+            'type' => 'mail',
+            'url' => 'mailto:nekimail@gmail.com',
+        ],
+    ],
+
+    'items' => [
+        [
+            'text' => 'Home',
+            'url' => 'https://www.youtube.com/watch?v=4mI33pHBO9g&ab_channel=ChilliLofi',
+            'class' => 'home-btn button',
+        ],
+        [
+            'text' => 'About us',
+            'url' => 'https://www.youtube.com/watch?v=4mI33pHBO9g&ab_channel=ChilliLofi',
+            'class' => 'about-us-btn button',
+        ],
+        [
+            'text' => 'Contact',
+            'url' => 'https://www.youtube.com/watch?v=4mI33pHBO9g&ab_channel=ChilliLofi',
+            'class' => 'contact-btn button',
+        ],
+        [
+            'text' => 'Locations',
+            'url' => '#',
+            'class' => 'button locations-button',
         ],
     ],    
 ];
-
