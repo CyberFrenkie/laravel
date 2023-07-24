940c310fdfb4b11c9946a5aa7d97f58c6bd30fac
Peconji <pecapeconji@gmail.com>
2023-07-25 00:56:31 +0200

    deleted 2nd HW

--- laravel-bootcamp/resources/views/petar.blade.php
+++ laravel-bootcamp/resources/views/petar.blade.php
@@ -3,36 +3,22 @@
 @section('content')
     
 <div class="wrapper">
-    <div style="text-align:center;">
-        <h1>{!! trans('buttons.content.title') !!}</h1>
-        @foreach(__('buttons.content.text') as $lyrics)
-            <p style="font-size:25px;">{{ $lyrics['lyrics'] }}</p>
-            <br>
-        @endforeach
+    <!-- 3rd Homework -->
+    <div class="pre-header">
+        <!-- dodao sam contact_tel -->
+        <a href="{{ __('buttons.pre-headerContact.contact_tel.url') }}" class="{{ __('buttons.pre-headerContact.contact_tel.type') }} phone">{{ __('buttons.pre-headerContact.contact_tel.text') }}</a>
+        <!-- dodao sam contact_mail -->
+        <a href="{{ __('buttons.pre-headerContact.contact_mail.url') }}" class="{{ __('buttons.pre-headerContact.contact_mail.type') }} mail">{{ __('buttons.pre-headerContact.contact_mail.text') }}</a>
     </div>
-    <div>
-        @foreach(__('buttons.content.buttons') as $button)
-            <a style="display:flex;justify-content:center;font-size:25px;color:orange;text-decoration:none;" href="{{ $button['url'] }}" target="{{ $button['target'] }}" class="{{ $button['class'] }}">{{ $button['text'] }}</a>
-        @endforeach
-   </div>
-</div>
-
-<!-- 3rd Homework -->
-<div class="pre-header">
-    <!-- dodao sam contact_tel -->
-    <a href="{{ __('navbar.pre-headerContact.contact_tel.url') }}" class="{{ __('navbar.pre-headerContact.contact_tel.type') }} phone">{{ __('navbar.pre-headerContact.contact_tel.text') }}</a>
-    <!-- dodao sam contact_mail -->
-    <a href="{{ __('navbar.pre-headerContact.contact_mail.url') }}" class="{{ __('navbar.pre-headerContact.contact_mail.type') }} mail">{{ __('navbar.pre-headerContact.contact_mail.text') }}</a>
-</div>
-<header class="header">
-    <div class="nav">
-        <div class="logo">
-            <img src="#" alt="PlaceHolder">
+    <header class="header">
+        <div class="nav">
+            <div class="logo">
+                <img src="#" alt="PlaceHolder">
+            </div>
+            @foreach ( __('buttons.items') as $item)
+                <a href="{{ $item['url'] }}" class="{{ $item['class'] }}">{{ $item['text'] }}</a>
+            @endforeach
         </div>
-        @foreach ( __('navbar.items') as $item)
-            <a href="{{ $item['url'] }}" class="{{ $item['class'] }}">{{ $item['text'] }}</a>
-        @endforeach
-
     </div>
 </header>
 
