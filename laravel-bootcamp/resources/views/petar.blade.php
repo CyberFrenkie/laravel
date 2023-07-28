70c06d654dbab9e0629ef07d85b638678d28be7a
Peconji <pecapeconji@gmail.com>
2023-07-29 00:26:32 +0200

    navbar4_3rdHW-created

--- laravel-bootcamp/resources/views/petar.blade.php
+++ laravel-bootcamp/resources/views/petar.blade.php
@@ -5,22 +5,31 @@
 <div class="wrapper">
     <!-- 3rd Homework -->
     <div class="pre-header">
-        <!-- dodao sam contact_tel -->
-        <a href="{{ __('buttons.pre-headerContact.contact_tel.url') }}" class="{{ __('buttons.pre-headerContact.contact_tel.type') }} phone">{{ __('buttons.pre-headerContact.contact_tel.text') }}</a>
-        <!-- dodao sam contact_mail -->
-        <a href="{{ __('buttons.pre-headerContact.contact_mail.url') }}" class="{{ __('buttons.pre-headerContact.contact_mail.type') }} mail">{{ __('buttons.pre-headerContact.contact_mail.text') }}</a>
+    @foreach( __('buttons.pre-headerContact') as $preheader)
+    <ul>
+        <li class="list">
+            <a href="{{ $preheader['url'] }}" class="{{ $preheader['type'] }}">{{ $preheader['text'] }}</a>
+        </li>
+    </ul>
+    @endforeach 
     </div>
-    <header class="header">
+    <div class="header">
         <div class="nav">
             <div class="logo">
-                <img src="#" alt="PlaceHolder">
+                <a href="#">
+                    <img src="img/logo-removebg-preview.png" alt="Logo" class="logo-img">
+                </a>
             </div>
             @foreach ( __('buttons.items') as $item)
-                <a href="{{ $item['url'] }}" class="{{ $item['class'] }}">{{ $item['text'] }}</a>
+            <ul>
+                <li class="list">
+                    <a href="{{ $item['url'] }}" class="{{ $item['class'] }}">{{ $item['text'] }}</a>
+                </li>
+            </ul>
             @endforeach
         </div>
     </div> 
-</header>
+</div>
 
 @endsection
 
